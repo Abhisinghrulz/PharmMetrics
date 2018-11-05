dd40c5fe2e0385bad4ed54ce2b6e84dd59d5d484
Abhishek <abhisinghrulz@gmail.com>
2018-11-05 23:47:07 +0530

    My Initial Commit

---
+++ src/main/java/com/pharmMetrics/PharmMetrics/PharmMetricsApplication.java
@@ -0,0 +1,12 @@
+package com.pharmMetrics.PharmMetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmMetricsApplication.class, args);
+	}
+}
